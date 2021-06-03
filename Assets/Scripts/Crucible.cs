1d447bdb11f42050c5f3c6c2395108ea09cd785c
Josh Luong <1021857@student.sae.edu.au>
2021-06-03 20:34:01 +1000

    Made crucible animation in JoshTesting scene

--- Assets/Scripts/Crucible.cs
+++ Assets/Scripts/Crucible.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Crucible : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    Animator anim;
+
+    private void Awake() {
+        anim = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if( Input.GetKey("a") ) {
+            anim.SetBool("PlayerInput", true);
+        } else {
+            anim.SetBool("PlayerInput", false);
+        }
     }
 }
