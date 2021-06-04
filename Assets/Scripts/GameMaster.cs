ce6e00aeb32de2e6852e7ca75f47a9a65b4b96a8
Josh Luong <1021857@student.sae.edu.au>
2021-06-04 16:26:10 +1000

    Pushing minor changes

--- Assets/Scripts/GameMaster.cs
+++ Assets/Scripts/GameMaster.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GameMaster : MonoBehaviour
 {
+    int lives;
+    static GameMaster masterInstance;
+    int lastGame;
+    int currentGame;
+
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        DontDestroyOnLoad(this.gameObject);
+        if( masterInstance == null ) {
+            masterInstance = this;
+        } else {
+            Object.Destroy(this.gameObject);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void ChangeGame() {
+        currentGame = Random.Range(0, 2);
+        if ( currentGame == lastGame ) {
+            return;
+        }
+        Debug.Log("Game chosen is " + currentGame);
+    }
+
+    public void GameOutcome(bool success ) {
+        if( !success ) {
+            lives -= 1;
+            if (lives <= 0) {
+
+            }
+        }
     }
 }
